e98f7ee4eee17ec42f7ccc481e310db53107156c
Suke0811 <49264928+Suke0811@users.noreply.github.com>
2023-01-06 11:42:06 -0800

    Initial commit

---
+++ GoatGasDriver/GoatGasDriver.py
@@ -0,0 +1,67 @@
+import time
+
+from ArduinoCommonApi import ArduinoCommonApi, PIN_MODE
+DEFAULT_PIN_IDs = [(0,2),(1,3),(2,4),(3,5)]#,(4,6),(5,7),(6,8),(7,9)]
+
+
+
+
+class GoatGasDriver:
+    def __init__(self, port, IDs, pins=None):
+        self.arduino = ArduinoCommonApi()
+        self.arduino.connect(port)
+        self.id_pin = self._create_id_pin_set(IDs, pins)
+        for i, p in self.id_pin:
+            self.arduino.set_pin_mode(p, PIN_MODE.OUTPUT)
+           # time.sleep(0.2)
+
+
+    def open_gripper(self, ID, open):
+        pin = self.get_pin_from_id(ID)
+        if open:
+            com = 'h'
+        else:
+            com = 'l'
+
+        self.arduino.write(pin, com)
+
+    def close(self):
+        self.arduino.close()
+
+    def _create_id_pin_set(self, IDs, pins):
+        if pins is None:
+            return DEFAULT_PIN_IDs
+
+        id_pin = []
+        for i, p in zip(IDs, pins):
+            id_pin.append((i, p))
+        return set(id_pin)
+
+
+    def get_pin_from_id(self, d_id):
+        for i, p in self.id_pin:
+            if i == d_id:
+                return p
+
+    def get_id_from_pin(self, pin):
+        for i, p in self.id_pin:
+            if p == pin:
+                return i
+
+
+
+if __name__ == '__main__':
+    g = GoatGasDriver('COM5', [0,1,2,3])
+    g.open_gripper(0, True)
+    # g.open_gripper(1, True)
+    # g.open_gripper(2, True)
+    # g.open_gripper(3, True)
+    for i in range(1000):
+        for n in range(4):
+            g.open_gripper(n, True)
+        time.sleep(0.001)
+        for n in range(4):
+            g.open_gripper(n, False)
+        time.sleep(0.001)
+
+    pass
